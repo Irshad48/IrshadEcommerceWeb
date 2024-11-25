308dfd4b1a8e24e2254152483b003658a1e8d53d
Irshad Shaikh <irshad.shaikh1992@outlook.com>
2024-11-25 17:31:16 +0530

    Completed DB Setup -> added connection string, EFCore setup, added migration, ceated  category table

---
+++ IrshadEcommerceWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using IrshadEcommerceWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IrshadEcommerceWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+                
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
