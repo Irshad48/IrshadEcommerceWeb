ad46ed952327fd70579422cfc732f9c66ab4e68a
Irshad Shaikh <irshad.shaikh1992@outlook.com>
2024-12-02 10:23:04 +0530

    Validation - showing service side valiation and how to display at summary and field level

--- IrshadEcommerceWeb/Controllers/CategoryController.cs
+++ IrshadEcommerceWeb/Controllers/CategoryController.cs
@@ -23,6 +23,18 @@ namespace IrshadEcommerceWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
+            if(!string.IsNullOrEmpty(obj.Name) && obj.Name.All(char.IsDigit))
+            {
+                //first parameter is Key - is asp-for set in the view for the model property
+                //it will show error below field
+                ModelState.AddModelError("Name", "Name should not be only integer, Pls enter valid Name");
+            }
+            if (obj.DisplayOrder.ToString() == obj.Name)
+            {
+                //If key is blank and in view asp-validation-summary-view is MOdelOnly then it will display validation at summary level
+                //this validation just added as explain to show how it works- in real world, its not valid validation
+                ModelState.AddModelError("", "Name and DisplayOrder should not be same");
+            }
             if (ModelState.IsValid)
             {
 
