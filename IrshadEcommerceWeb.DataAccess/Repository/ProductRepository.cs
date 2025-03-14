bf497483177b104a369471535137404384fc5655
Irshad Shaikh <irshad.shaikh1992@gmail.com>
2025-03-14 18:27:58 +0530

    Product: Added Repository changes

---
+++ IrshadEcommerceWeb.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using IrshadEcommerce.DataAccess.Data;
+using IrshadEcommerce.DataAccess.Repository.IRepository;
+using IrshadEcommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IrshadEcommerce.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product obj)
+        {
+            _db.Products.Update(obj);
+        }
+
+    }
+}
