ccd7ba34ba15af5ad266965fbd6cd51eb4e4daea
Irshad Shaikh <irshad.shaikh1992@gmail.com>
2025-03-14 18:19:53 +0530

    Product: Added product data to db

--- IrshadEcommerceWeb.DataAccess/Data/ApplicationDbContext.cs
+++ IrshadEcommerceWeb.DataAccess/Data/ApplicationDbContext.cs
@@ -10,6 +10,7 @@ namespace IrshadEcommerce.DataAccess.Data
                 
         }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -17,6 +18,42 @@ namespace IrshadEcommerce.DataAccess.Data
                 new Category { Id = 1, Name = "Action", DisplayOrder=1},
                 new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
                 new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Title = "Title 1",
+                    Author = "Author 1",
+                    Description = "Description 1",
+                    ISBN = "ISBN 1",
+                    ListPrice = 99,
+                    Price50 = 90,
+                    Price100 = 80
+                },
+                new Product
+                {
+                    Id = 2,
+                    Title = "Title 2",
+                    Author = "Author 2",
+                    Description = "Description 2",
+                    ISBN = "ISBN 2",
+                    ListPrice = 990,
+                    Price50 = 900,
+                    Price100 = 800
+                },
+                new Product
+                {
+                    Id = 3,
+                    Title = "Title 3",
+                    Author = "Author 3",
+                    Description = "Description 3",
+                    ISBN = "ISBN 3",
+                    ListPrice = 9900,
+                    Price50 = 9000,
+                    Price100 = 8000
+                }
                 );
         }
     }
---
+++ IrshadEcommerceWeb.DataAccess/Migrations/20250314124845_AddedProductData.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace IrshadEcommerce.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedProductData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ISBN = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ListPrice = table.Column<double>(type: "float", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Price50 = table.Column<double>(type: "float", nullable: false),
+                    Price100 = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Author", "Description", "ISBN", "ListPrice", "Price", "Price100", "Price50", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Author 1", "Description 1", "ISBN 1", 99.0, 0.0, 80.0, 90.0, "Title 1" },
+                    { 2, "Author 2", "Description 2", "ISBN 2", 990.0, 0.0, 800.0, 900.0, "Title 2" },
+                    { 3, "Author 3", "Description 3", "ISBN 3", 9900.0, 0.0, 8000.0, 9000.0, "Title 3" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
